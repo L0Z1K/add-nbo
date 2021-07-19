60f0d03d5fc9238ae84d88255b563af0eaa16e9d
L0Z1K <qortmddbs731@naver.com>
2021-07-19 09:25:59 +0900

    fix: typo in logging message

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[]){
 
     if(argc != 3)
     {
-        printf("[*]Usage: ./add-nbo <file1> <file2>\n");
+        printf("[*] Usage: ./add-nbo <file1> <file2>\n");
         exit(0);
     }
 
